7971c1dbc54e0c80fedf6e8d4271a319337100c2
Wesley Xavier <79912798+WesleyXavier99@users.noreply.github.com>
2022-06-14 14:46:46 -0300

    Consulta para dados do pdf
    Somente os nomes de aluno e livro que não estão aparecendo no pdf, mas de resto tudo ok. A imagem no pdf já foi removida.

--- php/empPDF.php
+++ php/empPDF.php
@@ -1,84 +1,134 @@
 <?php
-$mat = $_GET["matricula"];
-$cod = $_GET["codigo"];
-$dataEMP = $_GET["dataEMP"];
-$dataDEV = $_GET["dataDev"];
-$id = $_GET["idEMP"];
+    $mat = $_GET["matricula"];
+    $cod = $_GET["codigo"];
+    $dataEMP = $_GET["dataEMP"];
+    $dataDEV = $_GET["dataDev"];
+    $id = $_GET["idEMP"];
 
+    /*Parametros de conexão com o banco*/
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $nomeBanco = "biblioteca";
 
-require_once "../dompdf/autoload.inc.php";
-use Dompdf\Dompdf;
-$pdf = new DomPdf();
-$pdf -> loadHtml("
-<head>
-<title>Recibo de empréstimo</title>
-<style>
-    .borda{
-        margin: 30px;
-        border: 5px solid black;
-        width: 600px;
-        margin-left: auto;
-        margin-right: auto;
+    /*Realizando conexão com o banco*/
+    $conn = new mysqli($servidor, $usuario, $senha, $nomeBanco);
+
+    if ($conn->connect_error) {
+        die("Não foi possível estabelecer uma conexão!" . $conn->connect_error);
     }
-    .topo{
-        border: 5px solid black;
-        height: 150px;
-        margin: 5px;
-        margin-left: auto;
-        margin-right: auto;
+
+    /*Buscando nome do aluno*/
+    $sql = "SELECT * FROM `alunos` WHERE `matricula`='$mat'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 0)
+    {
+        echo json_encode("Não existe aluno com a matrícula correspondente!");
     }
-    .dados{
-        border-collapse: collapse;
-        width: 570px;
-        margin: 15px;
+    else{
+        $linha = $result->fetch_assoc();
+        $nomeAluno = $linha["nome"];
     }
-    tr{
-        border: 2px solid #000000 ;
+
+    /*Buscando nome do livro*/
+    $sql = "SELECT * FROM `livros` WHERE `cod`='$cod'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 0)
+    {
+        echo json_encode("Não existe livro com o código correspondente!");
     }
-    .observacoes{
-        border: 5px solid #FFFFFF;
-        height: 100px;
-        margin: 15px;
+    else{
+        $linha = $result->fetch_assoc();
+        $nomeLivro = $linha["nome"];
+        $isbnLivro = $linha["isbn"];
     }
-</style>
-</head>
-<body>
-<div class='borda'>
-    <div class='topo'>
-            <figure>
-                <figcaption style='text-align: center'>Biblioteca Universitária</figcaption>
-                <img src='../imagens/1920478300008.jpg' width='65px' height='30px' style='text-align: center'>
-                <p style='text-align: center'>Recibo de empréstimo</p>
-                <p style='text-align: center' >Data de emissão: <?$dataEMP</p>
-            </figure>   
+
+    require_once "../dompdf/autoload.inc.php";
+    use Dompdf\Dompdf;
+    $pdf = new DomPdf();
+    $pdf -> loadHtml("
+    <head>
+    <title>Recibo de empréstimo</title>
+    <style>
+        .borda{
+            margin: 30px;
+            border: 5px solid black;
+            width: 600px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .topo{
+            border-bottom: 5px solid black;
+            height: 150px;
+            margin: 5px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .dados{
+            border-collapse: collapse;
+            width: 570px;
+            margin: 15px;
+        }
+        tr{
+            border: 2px solid #000000 ;
+        }
+        .observacoes{
+            border: 5px solid #FFFFFF;
+            height: 100px;
+            margin: 15px;
+        }
+    </style>
+    </head>
+    <body>
+    <div class='borda'>
+        <div class='topo'>
+                <figure>
+                    <figcaption style='text-align: center'>Biblioteca Universitária</figcaption>
+                    <p style='text-align: center'>Recibo de empréstimo</p>
+                    <p style='text-align: center' >Data de emissão: <?$dataEMP</p>
+                </figure>   
+        </div>
+        <table class='dados'>
+            <tr>
+                <th>Código do empréstimo:</th>
+                <th><?$id</th>
+            </tr>
+            <tr>
+                <th>Nome do Aluno:</th>
+                <th><?$nomeAluno</th>
+            </tr>
+            <tr>
+                <th>Matrícula:</th>
+                <th><?$mat</th>
+            </tr>
+            <tr>
+                <th>Nome do Livro:</th>
+                <th><?$nomeLivro</th>
+            </tr>
+            <tr>
+                <th>Isbn</th>
+                <th><?$isbnLivro</th>
+            </tr>
+            <tr>
+                <th>Data de empréstimo:</th>
+                <th><?$dataEMP</th>
+            </tr>
+            <tr>
+                <th>Data de devolução</th>
+                <th><?$dataDEV</th>
+            </tr>           
+        </table>
+        <div class='observacoes'>
+            <p>Obs: Caso a devolução seja feita além do prazo, o aluno fica ciente de que estará sujeito a restrições em futuros empréstimos!</p>
+        </div>
     </div>
-    <table class='dados'>
-        <tr>
-            <th>Código do empréstimo:</th>
-            <th><?$id</th>
-        </tr>
-        <tr>
-            <th>Matrícula:</th>
-            <th><?$mat</th>
-        </tr>
-        <tr>
-            <th>Data de empréstimo:</th>
-            <th><?$dataEMP</th>
-        </tr>
-        <tr>
-            <th>Data de devolução</th>
-            <th><?$dataDEV</th>
-        </tr>
-    </table>
-    <div class='observacoes'>
-        <p>Obs: Caso a devolução seja feita além do prazo, o aluno fica ciente de que estará sujeito a restrições em futuros empréstimos!</p>
-    </div>
-</div>
-</body>
-</html>");
+    </body>
+    </html>");
 
-$pdf->render();
-$pdf->stream("Empréstimo.pdf",array("Attachment" => false));
+    $pdf->render();
+    $pdf->stream("Empréstimo.pdf",array("Attachment" => false));
 
 
 ?>
